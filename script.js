197c40fa184060b7d8bb9c6c3ebecb1fdc82fc14
Sofi <sofi@gmail.com>
2022-06-22 13:50:27 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector(".hamburger-menu").addEventListener("click",()=> {
+    document.querySelector(".container").classList.toggle("change");
+});
+
+
+document.querySelector('.scroll-btn').addEventListener('click',()=>{
+    document.querySelector('html').style.scrollBehavior='smooth';
+    setTimeout(()=> {
+        document.querySelector("html").style.scrollBehavior="smooth";
+    },1000); 
+});
